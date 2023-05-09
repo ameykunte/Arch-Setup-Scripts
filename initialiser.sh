c903d672e91d84ae9940ed36dfd8e98f0174a438
Amey Kunte <amey.k@research.iiit.ac.in>
2023-05-09 06:18:29 +0530

    Updated initialiser.sh with progress bar

--- initialiser.sh
+++ initialiser.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+# Function to install snapd package manager
+function install_snapd {
+    echo "Installing snapd..."
+    sudo pacman -S snapd --noconfirm
+    sudo systemctl enable --now snapd.socket
+    sudo ln -s /var/lib/snapd/snap /snap
+}
 
 # Function to install snap packages
 function install_snap_packages {
@@ -6,70 +15,70 @@ function install_snap_packages {
     read -p "Install Discord? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install discord
+        sudo snap install discord | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Notion? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install notion-snap
+        sudo snap install notion-snap | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Spotify? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install spotify
+        sudo snap install spotify | pv -p -t -e >/dev/null
     fi
 
     read -p "Install WhatsApp for Linux? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install whatsapp-for-linux
+        sudo snap install whatsapp-for-linux | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Teams? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install teams
+        sudo snap install teams | pv -p -t -e >/dev/null
     fi
 
     read -p "Install VLC? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install vlc
+        sudo snap install vlc | pv -p -t -e >/dev/null
     fi
 
     read -p "Install Prospect Mail? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install prospect-mail
+        sudo snap install prospect-mail | pv -p -t -e >/dev/null
     fi
 
     read -p "Install VS Code? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        sudo snap install code --classic
+        sudo snap install code --classic | pv -p -t -e >/dev/null
     fi
 }
 
 # Function to install basic tools
 function install_basic_tools {
     echo "Installing basic tools..."
-    sudo pacman -S --noconfirm git curl wget base-devel
-    sudo pacman -S --noconfirm python-pip
-    sudo pacman -S --noconfirm nodejs npm
+    sudo pacman -S git curl wget base-devel python python-pip nodejs npm --noconfirm | pv -p -t -e >/dev/null
 }
 
 # Function to update the system
 function update_system {
     echo "Updating the system..."
-    sudo pacman -Syu --noconfirm
+    sudo pacman -Syu --noconfirm | pv -p -t -e >/dev/null
+    sudo pacman -S preload htop tlp --noconfirm | pv -p -t -e >/dev/null
+    sudo systemctl enable --now preload tlp
 }
 
 # Function to clean up the system
 function clean_up {
     echo "Cleaning up the system..."
-    sudo pacman -Rns --noconfirm $(pacman -Qtdq)
+    sudo pacman -Rns $(pacman -Qtdq) --noconfirm | pv -p -t -e >/dev/null
 }
 
 # Main function
